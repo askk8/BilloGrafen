6a72f2aab0e6009b3c48fe2600d742a27464b048
Adam <adamprojektet@gmail.com>
2019-10-21 14:46:14 +0200

    Karusell/slide på startsidan

---
+++ Frontend/src/components/Slide.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Slide } from 'react-slideshow-image'
+import './Slide.css'
+
+const proprietes = {
+    duration: 5000,
+    transitionDuration: 500,
+    infinite: true,
+    indicators: true,
+    arrows : true
+}
+
+const Slideshow = () => {
+    return (
+        <div className="containerSlide">
+            <Slide {...proprietes}>
+                <div className="each-slide">
+                    <div>
+                        <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/xcD84rH2f1dytTFeJlopoJ5MKgW.jpg" />
+                    </div>
+                </div>
+                <div className="each-slide">
+                    <div>
+                        <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/k0jeyTsYzDWlY6FLfBYOmeShNRw.jpg" />
+                    </div>
+                </div>
+                <div className="each-slide">
+                    <div>
+                        <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/ueC9OZEJF98gbPI0RsKaR821qX9.jpg" />
+                    </div>
+                </div>
+                <div className="each-slide">
+                    <div>
+                        <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/ePXuKdXZuJx8hHMNr2yM4jY2L7Z.jpg" />
+                    </div>
+                </div>
+                <div className="each-slide">
+                    <div>
+                        <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/ogFqy52mfhnlEP7pr5rUCzwngnB.jpg" />
+                    </div>
+                </div>
+            </Slide>
+        </div>
+
+    )
+}
+export default Slideshow;
--- Frontend/src/components/homepage.js
+++ Frontend/src/components/homepage.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
 import { Grid, Cell} from 'react-mdl';
+import Slideshow from './Slide.js';
 
 class HomePage extends Component {
     render() {
         return (
         <div className="home-grid">
-            <Grid>
-                <Cell col={12}>
-                    <img
-                        src="husse.jpg"
-                        alt="avatar"
-                        className="avatar-img"
-                        />
-
-                        <div className="banner-text">
-                            <h1>Husse In The Building</h1>
-                        </div>
-                </Cell>
-            </Grid>
+            <h3>ToppListan</h3>
+                    <Slideshow />
         </div>
         )
     }
