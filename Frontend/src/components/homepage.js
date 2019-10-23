069cc68851625fa7e92c850690d04001a2473642
Adam <adamprojektet@gmail.com>
2019-10-23 17:47:40 +0200

    Ändringar i typsnitt och Om oss sidan

--- Frontend/src/components/homepage.js
+++ Frontend/src/components/homepage.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { Grid, Cell} from 'react-mdl';
 import Slideshow from './Slide.js';
 
 class HomePage extends Component {
     render() {
         return (
         <div className="home-grid">
-            <h3>ToppListan</h3>
+            <h3>𝕋𝕠𝕡𝕡𝕝𝕚𝕤𝕥𝕒𝕟</h3>
                     <Slideshow />
         </div>
         )
--- Frontend/src/components/omoss.js
+++ Frontend/src/components/omoss.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { Jumbotron, Grid, Row, Col, Image, Button} from 'react-bootstrap';
+import './Omoss.css';
+
 
 class OmOss extends Component {
     render() {
         return (
-        <div className="home-grid"><h1>Om oss Sida</h1></div>
+        <div className="home-grid">
+            <img src="friend.jpg" width="100%" height="40%"/>
+            <h1></h1>
+            <img src="Adam cropped.jpg"  />
+            <img src="Arian cropped.jpg"  />
+            <img src="Bill cropped.jpg"  />
+
+            </div>
         )
     }
 }
